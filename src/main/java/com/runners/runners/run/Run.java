a8eece297f5a9926fb39922c8d99a777cc9a3537
Pubudu Gunasekara <pubudupraneeth21@gmail.com>
2024-07-29 03:17:20 -0400

    CRUD API boilerplate

--- src/main/java/com/runners/runners/run/Run.java
+++ src/main/java/com/runners/runners/run/Run.java
@@ -1,12 +1,23 @@
 package com.runners.runners.run;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public record Run(Integer id,
+                  @NotEmpty
                   String title,
                   LocalDateTime startedOn,
                   LocalDateTime completedOn,
+                  @Positive
                   Integer miles,
                   Location location
-                  ) {}
+                  ) {
+    public Run{
+        if(!completedOn.isAfter(startedOn)){
+            throw new IllegalArgumentException("Completed on must be after started on");
+        }
+    }
+}
--- src/main/java/com/runners/runners/run/RunController.java
+++ src/main/java/com/runners/runners/run/RunController.java
@@ -1,9 +1,9 @@
 package com.runners.runners.run;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,15 +17,41 @@ public class RunController {
         this.runRepository = runRepository;
     }
 
+    //get all
     @GetMapping("")
     List<Run> findAll(){
       return runRepository.findAll();
   }
 
+  //get by id
     @GetMapping("/{id}")
-    Optional<Run> findById(@PathVariable Integer id){
-        return runRepository.findById(id);
+    Run findById(@PathVariable Integer id){
+        Optional<Run> run = runRepository.findById(id);
+        if(run.isEmpty()){
+            throw new RunNotFoundException();
+        }
+        return run.get();
     }
 
+    //post
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    void create(@Valid @RequestBody Run run){
+        runRepository.create(run);
+    }
+
+    //put
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    void update(@Valid @RequestBody Run run, @PathVariable Integer id){
+        runRepository.update(run,id);
+    }
+
+    //delete
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Integer id){
+        runRepository.delete(id);
+    }
 
 }
